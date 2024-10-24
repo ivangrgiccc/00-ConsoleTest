3075b3b29d11951b6cc2c92886df5d390677b26b
ivan0301 <ivangrgic0301@gmail.com>
2024-10-24 17:59:46 +0200

    dodano ime

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!test tset test");
+            Console.WriteLine("Ivan Grgic");
         }
     }
 }
